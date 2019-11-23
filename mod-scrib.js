a2b00c2a64a99cb7e612620d7941bd720d9f8ef7
tiedurden <40056575+tiedurden@users.noreply.github.com>
2019-11-24 00:28:29 +0100

    Add files via upload

---
+++ mod-scrib.js
@@ -0,0 +1,9 @@
+function findTotal() {
+    var arr = document.getElementsByName("qty");
+    var tot = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+    }
+    document.getElementById("countertotal").value = tot;
+}
